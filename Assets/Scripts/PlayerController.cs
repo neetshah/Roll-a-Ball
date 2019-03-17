3cc795dcfe2013dbe2bcdec2190200d7331becc9
Neet Shah <shahn17@mcmaster.ca>
2019-03-17 17:48:27 -0400

    Receive input from user

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+    }
+}
