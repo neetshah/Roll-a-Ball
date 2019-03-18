b493cebc52d7239d93a0eccd8f79018bc51f7014
Neet Shah <shahn17@mcmaster.ca>
2019-03-18 02:26:39 -0400

    Add score ui

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,9 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
     public float speed;
+    private int count;
 
     void Start() {
         rb = GetComponent<Rigidbody>();
+        count = 0;
     }
 
     void FixedUpdate() {
@@ -18,5 +20,11 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical  );
 
         rb.AddForce (movement * speed);
+    }
+    void OnTriggerEnter(Collider other) {
+        if(other.gameObject.CompareTag("Pick Up")) {
+            other.gameObject.SetActive(false);
+            count +=1;
+        }
     }
 }
