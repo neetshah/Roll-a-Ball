b23f23ea5f14605d6ef55dbdc6cdf9f7ceffc2a0
Neet Shah <shahn17@mcmaster.ca>
2019-03-18 23:19:24 -0400

    Add win text

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,30 +1,50 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 
-public class PlayerController : MonoBehaviour
-{
-    private Rigidbody rb;
+public class PlayerController : MonoBehaviour {
+
     public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
     private int count;
 
-    void Start() {
+    void Start ()
+    {
         rb = GetComponent<Rigidbody>();
         count = 0;
+        SetCountText ();
+        winText.text = "";
     }
 
-    void FixedUpdate() {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical  );
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce (movement * speed);
     }
-    void OnTriggerEnter(Collider other) {
-        if(other.gameObject.CompareTag("Pick Up")) {
-            other.gameObject.SetActive(false);
-            count +=1;
+
+    void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.CompareTag ( "Pick Up"))
+        {
+            other.gameObject.SetActive (false);
+            count = count + 1;
+            SetCountText ();
+        }
+    }
+
+    void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString ();
+        if (count >= 10)
+        {
+            winText.text = "You Win!";
         }
     }
 }
